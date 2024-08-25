778c8b650d82c3e9850ff7824a99be8eae58e6a5
Legion <babakmustafayev@gmail.com>
2024-08-25 23:40:24 +0400

    Switch to ICommand interface and implement product creation
    Refactored CreateProductCommand and handler to use ICommand instead of IRequest interface. Added logic to map command data to a Product entity and provided a placeholder for saving to the database.

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandle.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandle.cs
@@ -1,16 +1,39 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct;
 
-public record CreateProductCommand(string Name,List<string> Category, string Description,string ImageFile, decimal Price) :IRequest<CreateProductResult>;
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price)
+    : ICommand<CreateProductResult>;
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandle: IRequestHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandle 
+    : ICommandHandler<CreateProductCommand, CreateProductResult>
 {
-    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
         //Business logic to create a product
-        throw new NotImplementedException();
+        //Create Product entity from command object 
+               
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            Description = command.Description,
+            ImageFile = command.ImageFile,
+            Price = command.Price
+        };
+        
+        //TODO
+        //save to database
+        //return CreateProductResult result
+        return new CreateProductResult(new Guid());
     }
 }
