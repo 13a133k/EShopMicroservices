505db675a2ffb4edfe5bdc9479c8944554acdd2e
Legion <babakmustafayev@gmail.com>
2024-09-01 01:50:52 +0400

    Add GetProductByCategory endpoint and query handler
    Introduce a new endpoint to fetch products by category. Implement the corresponding query, result model, and handler to process the requests and return the products.

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Catalog.API.Products.GetProductByCategory;
+
+//public record GetProductByCategoryRequest();
+
+public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+
+public class GetProductByCategoryEndpoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductByCategoryQuery(category));
+
+            var response = result.Adapt<GetProductByCategoryResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetProductByCategoryEndpoint")
+        .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status204NoContent)
+        .WithSummary("GetProductByCategoryEndpoint")
+        .WithDisplayName("GetProductByCategoryEndpoint");
+    }
+}
