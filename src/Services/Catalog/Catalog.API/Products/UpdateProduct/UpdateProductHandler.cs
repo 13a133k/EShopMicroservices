5150b7b8ea3c86559af3b23d3230ade55568c311
Legion <babakmustafayev@gmail.com>
2024-09-02 02:27:58 +0400

    Add custom exception handling and refactor exceptions
    Implemented `CustomExceptionHandler` for centralized error handling. Introduced `NotFoundException`, `BadRequestException`, and `InternalServerException` for more specific exception handling. Updated `ProductNotFoundException` to include product ID for better debugging.

--- src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/UpdateProduct/UpdateProductHandler.cs
@@ -42,7 +42,7 @@ internal class UpdateProductCommandHandler(IDocumentSession session, ILogger<Upd
 
         if (product is null)
         {
-            throw new ProductNotFoundException();
+            throw new ProductNotFoundException(command.Id);
         }
 
         product.Name = command.Name;
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,3 @@
-using BuildingBlocks.Behaviors;
-
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
@@ -17,9 +15,13 @@ builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddMarten(opts => { opts.Connection(builder.Configuration.GetConnectionString("Database")!); })
     .UseLightweightSessions();
 
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
 var app = builder.Build();
 
 //configure the Http request pipeline. 
 app.MapCarter();
+
+app.UseExceptionHandler(options => { });
 
 app.Run();
