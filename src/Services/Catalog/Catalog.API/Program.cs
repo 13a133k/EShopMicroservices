1973135b86810e4094421fe76e6128c14db697f9
Legion <babakmustafayev@gmail.com>
2024-09-03 02:08:17 +0400

    Refactor logging behavior
    Renamed ValidationBehaviors to ValidationBehavior and removed logging in command/query handlers. Introduced LoggingBehavior for centralized logging and added it to the MediatR pipeline in Program.cs.

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -7,7 +7,8 @@ var assembly = typeof(Program).Assembly;
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssembly(assembly);
-    config.AddOpenBehavior(typeof(ValidationBehaviors<,>));
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 
 builder.Services.AddValidatorsFromAssembly(assembly);
