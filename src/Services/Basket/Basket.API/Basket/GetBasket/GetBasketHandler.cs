91a8a536558ace750e77f81085a3e88fc134f09c
Legion <babakmustafayev@gmail.com>
2024-09-30 01:12:45 +0400

    Add initial Basket API handlers and endpoints
    This commit introduces skeleton classes for handling basket operations including Get, Store, and Delete actions within the Basket API.

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,16 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string Username) : IQuery<GetBasketResult>;
+
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketQuerHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        //todo: get basket from database
+        //var basket = await _repository.GetBasket(query.UserName);
+
+        return new GetBasketResult(new ShoppingCart("swn"));
+    }
+}
