a478d369a35e5b79829d563ad9903f36dec09d2b
Legion <babakmustafayev@gmail.com>
2024-10-22 00:03:57 +0400

    Add SQLite support and Coupon model to Discount.Grpc. Integrated SQLite with Entity Framework Core into Discount.

---
+++ src/Services/Discount.Grpc/Data/DiscountContext.cs
@@ -0,0 +1,24 @@
+﻿using Discount.Grpc.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data;
+
+public class DiscountContext : DbContext
+{
+    public DbSet<Coupon> Coupons { get; set; } = default!;
+
+    public DiscountContext(DbContextOptions<DiscountContext> options) 
+        : base(options)
+    {
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Coupon>().HasData(
+            new Coupon{ Id = 1,ProductName = "Iphone X",Description = "Iphone details",Amount = 1500},
+            new Coupon{ Id = 2,ProductName = "Samsung S21",Description = "Iphone details",Amount = 1450} 
+        );
+    }
+
+}
