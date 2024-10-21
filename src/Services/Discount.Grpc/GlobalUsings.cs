d8e2ea9f0827d41c98e0fedfd2957b172c210ede
Legion <babakmustafayev@gmail.com>
2024-10-22 00:46:24 +0400

    Add Mapster library and implement Discount CRUD operations

---
+++ src/Services/Discount.Grpc/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿// Global using directives
+
+global using Discount.Grpc.Data;
+global using Discount.Grpc.Model;
+global using Grpc.Core;
+global using Mapster;
+global using Microsoft.EntityFrameworkCore;
