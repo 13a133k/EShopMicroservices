15c384d7cde9c0167dcd46d6266a6095bca06bd7
Legion <babakmustafayev@gmail.com>
2024-10-28 02:38:32 +0400

    Add gRPC Discount service configuration
    Configured the Basket API to communicate with the Discount gRPC service, including settings and server certificate validation. Removed old database files from the discount service.

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,4 @@
+using System.Net.Security;
 using Discount.Grpc;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
@@ -43,9 +44,19 @@ builder.Services.AddStackExchangeRedisCache(options =>
 
 //Grpc Services
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(options =>
-{
-    options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
-});
+    {
+        options.Address = new Uri(builder.Configuration["GrpcSettings:DiscountUrl"]!);
+    })
+    .ConfigurePrimaryHttpMessageHandler(() =>
+        {
+            var handler = new HttpClientHandler
+            {
+                ServerCertificateCustomValidationCallback =
+                    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+            };
+            return handler;
+        }
+    );
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
