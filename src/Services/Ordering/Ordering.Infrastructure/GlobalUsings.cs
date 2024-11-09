503fd80e79ccdabda4fb5afbd9c293bbe771451d
Legion <babakmustafayev@gmail.com>
2024-11-09 11:12:52 +0400

    Add Entity Framework Core configurations
    Introduce EF Core configurations for Order, OrderItem, Customer, and Product.

---
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿// Global using directives
+
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Ordering.Domain.Enums;
+global using Ordering.Domain.Models;
+global using Ordering.Domain.ValueObjects;
