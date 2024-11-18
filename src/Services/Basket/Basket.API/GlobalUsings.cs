6672aedb267777d7cb7442013e21bb0b4768f851
Legion <babakmustafayev@gmail.com>
2024-11-18 04:16:20 +0400

    Add basket checkout functionality
    Implemented a handler and endpoints for basket checkout. Introduced `BasketCheckoutDto` for checkout details and validation rules. Integrated messaging to handle basket checkout events and basket deletion post-checkout.

--- src/Services/Basket/Basket.API/GlobalUsings.cs
+++ src/Services/Basket/Basket.API/GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿global using System.Text.Json;
 global using Basket.API.Data;
+global using Basket.API.Dtos;
 global using Basket.API.Exceptions;
 global using Basket.API.Models;
 global using BuildingBlocks.Behaviors;
