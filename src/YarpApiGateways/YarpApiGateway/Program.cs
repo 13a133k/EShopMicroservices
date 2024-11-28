67d69c46ee8382fbd1de361e4088bfc4337844e8
Legion <babakmustafayev@gmail.com>
2024-11-28 06:33:30 +0400

    Add rate limiting and update service addresses
    Implemented a fixed window rate limiter with a 10-second window and 5 request limit per window. Updated configuration to apply rate limiter to the ordering route and modified service addresses to point to the proper API endpoints. Added a local settings file for development purposes.

--- src/YarpApiGateways/YarpApiGateway/Program.cs
+++ src/YarpApiGateways/YarpApiGateway/Program.cs
@@ -1,12 +1,51 @@
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+builder.Services.AddRateLimiter(rateLimiterOptions =>
+{
+    rateLimiterOptions.AddFixedWindowLimiter("fixed", options =>
+    {
+        options.Window = TimeSpan.FromSeconds(10);
+        options.PermitLimit = 5;
+    });
+
+    #region Other Rate limiter Options
+
+    // rateLimiterOptions.AddSlidingWindowLimiter("SlidingWindow", options =>
+    // {
+    //     options.Window = TimeSpan.FromSeconds(10);
+    //     options.SegmentsPerWindow = 5;
+    // });
+    
+    //      rateLimiterOptions.AddTokenBucketLimiter("Bucket", options =>
+    //  {
+    //      options.QueueLimit =11;
+    //      options.TokenLimit = 5;
+    //  });
+    
+    // rateLimiterOptions.AddConcurrencyLimiter("Concurrency", options =>
+    // {
+    //     options.PermitLimit = 12;
+    //     options.PermitLimit = 5;
+    // });
+
+    #endregion
+});
+
+
+builder.Services.AddRateLimiter(rateLimiterOptions => { });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+app.UseRateLimiter();
 
 app.MapReverseProxy();
 
